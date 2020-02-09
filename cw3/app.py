b168e94dd89dc692514976b899ed684b4b00e318
zsooti25 <zsooti25@github.com>
2020-02-09 23:47:51 +0000

    asd

--- cw3/app.py
+++ cw3/app.py
@@ -1,12 +1,4 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app = create_app()
+app.app_context().push()
